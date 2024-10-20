f37128bfe282390177c2f382ca1d3e5592a9df12
Sherwin <87555304+owenlim225@users.noreply.github.com>
2024-10-20 19:58:05 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+class Gameplay:
+    def __init__(self) -> None:
+        pokemon_list = [
+            ["Bulbasaur", 100, 60],
+            ["Charmander", 100, 55],
+            ["Eevee", 100, 52],
+            ["Gengar", 100, 70],
+            ["Jigglypuff", 100, 45],
+            ["Machamp", 100, 75],
+            ["Mewtwo", 100, 90],
+            ["Pikachu", 100, 50],
+            ["Snorlax", 100, 80],
+            ["Squirtle", 100, 58]
+        ]
+
+
+
+
+
+
+'''
+Functions I need:
+> 1 for storing the pokemons 
+> 1 for player to access the data of the pokemons + removing the picked pokemon by player 1
+'''
